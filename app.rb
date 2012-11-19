12ba92237fcbae1e4c888717848a44d7c85596c9
Charlie Somerville <charlie@charliesomerville.com>
2012-11-20 00:03:20 +1100

    make code regex work on code at the end of a string too

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ helpers do
   end
   
   def format_post(source)
-    markdown source.gsub(/^    \\[a-z]+\s*\n(    .*\n)*/) { |snippet|
+    markdown source.gsub(/^    \\[a-z]+\s*\n(    .*(\n|$))*/) { |snippet|
       lang, *source = snippet.lines.to_a
       Pygments.highlight source.map { |x| x[4..-1] }.join("\n"), lexer: lang[5..-1].strip, options: { encoding: "utf-8" }
     }

0f2b538060aa541e267de86218bebe8bb3eb9863
Charlie Somerville <charlie@charliesomerville.com>
2014-08-06 17:20:30 -0400

    import current revision of the site

---
+++ cgi-bin/bogan-ipsum.rb
@@ -0,0 +1,11 @@
+#!/opt/rubies/ruby-2.1.2/bin/ruby
+require "open-uri"
+require "nokogiri"
+
+print "Content-Type: text/html\n\n"
+
+doc = Nokogiri::XML(open("http://www.boganipsum.com/").read)
+
+sentences = doc.css(".bogan-ipsum p").flat_map { |p| p.text.split(/(?<=\. )/) }
+
+print sentences.sample.gsub(/\s+/, " ").strip
